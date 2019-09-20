bca94cf3e09d6f05fb7d56bf578d8170fb3b12c1
Kaushik <737144@cognizant.com>
2019-09-20 13:03:10 +0530

    ui tweaks

--- frontend/seatBookingUi/src/app/booking/booking.component.ts
+++ frontend/seatBookingUi/src/app/booking/booking.component.ts
@@ -19,12 +19,18 @@ export class BookingComponent implements OnInit {
   alreadyBookedSeats:any;
   seat = Array<number>(80);
   ngOnInit(): void {
+    localStorage.clear();
     this.bookingService.alreadyBookedSeats().subscribe((res:Seat)=>{
       this.alreadyBookedSeats = res;
       this.alreadyBooked1();
     })
   }
   bookSeats(){
+    if(this.seats>7||this.seats==0)
+    {
+      alert("Please enter valid no seats your want to book it should be in range of 1 to 7");
+
+    }else{
     this.bookingService.bookseats(this.seats).subscribe((res)=>{
 
       this.currentBookedSeats = res;
@@ -32,8 +38,8 @@ export class BookingComponent implements OnInit {
       this.dataService.changeSeatStatus(this.seat);
       this.route.navigate(["/seatmap"]);
     },(error:HttpErrorResponse)=>{
-      console.log(error);
-    })
+      alert("Seats are full");
+    })}
   }
   currentlyBooked(){
     for (let index = 0; index < this.currentBookedSeats.length; index++) {
